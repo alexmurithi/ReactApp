b716e694ad35a8549197b91a7ec7ecf34b138ea5
Alex Murithi <gitsaleks@gmail.com>
2021-04-04 12:21:54 +0300

    useStateHook

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
-function SecretComponent(){
-  return <h1>This is a secret component</h1>
-}
-
-function RegularComponent(){
-  return <h1>This is a regular component</h1>
-}
-
-
-
-
-function App({authorized}) {
+function App() {
+  const [emotion, setEmotion] =useState("happy")
  return(
    <>
-   {authorized ? <RegularComponent /> : <SecretComponent />}
+   <h1>Current emotion is {emotion}</h1>
+   <button onClick={()=>setEmotion("happy")}>Happy</button>
+   <button onClick={()=>setEmotion("frustrated")}>Frustrate</button>
+   <button onClick={()=>setEmotion("Angry")}>Angry</button>
    </>
  )
  
 }
 
-
-const [protein,drink] =["eggs","soda","milk"]
-console.log(protein,drink)
-
-const [, drinks] =["eggs","soda","milk"]
-console.log(drinks)
 
 export default App;
